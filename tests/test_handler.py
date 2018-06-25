439ab0375b55bf9f0fde5a9f0025d4a03e190ed0
Franky Martin <fmartin@neutral.io>
2018-06-25 16:32:50 -0400

    NIO-1139, making use of configuration manager component

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,3 +1,4 @@
+import json
 from threading import Event
 from unittest.mock import MagicMock
 
@@ -24,10 +25,9 @@ class TestConfigHandler(NIOWebTestCase):
             return super().get_module(module_name)
 
     def test_on_get(self):
-        manager = MagicMock()
         mock_req = MagicMock(spec=Request)
         mock_req.get_identifier.return_value = 'not_refresh'
-        handler = ConfigHandler(None, None, None)
+        handler = ConfigHandler(None, None, None, None, None, None)
 
         # Verify error is raised with incorrect identifier
         request = mock_req
@@ -36,17 +36,75 @@ class TestConfigHandler(NIOWebTestCase):
             handler.on_get(request, response)
 
     def test_on_put(self):
-        manager = MagicMock()
         mock_req = MagicMock(spec=Request)
         mock_req.get_identifier.return_value = 'update'
-        handler = ConfigHandler(None, None, None)
+        config_manager = MagicMock()
+        start_stop_services = True
+        delete_missing = False
+        handler = ConfigHandler("url_prefix", None, None, config_manager,
+                                start_stop_services, delete_missing)
+        handler._proxy = MagicMock()
 
         # Verify error is raised with incorrect put body
-        mock_req.get_body.return_value  = {}
+        mock_req.get_body.return_value = {}
         request = mock_req
         response = MagicMock()
         with self.assertRaises(ValueError):
             handler.on_put(request, response)
+
+        mock_req.get_body.return_value = {
+            "instance_configuration_id": "inst_config_id",
+            "instance_configuration_version_id": "inst_config_version_id"
+        }
+        with self.assertRaises(RuntimeError):
+            handler.on_put(request, response)
+
+        blocks = {
+            "block_id": {
+                "id": "block_id"
+            }
+        }
+        services = {
+            "service_id": {
+                "id": "service_id"
+            }
+        }
+        handler._proxy.load_configuration.return_value = {
+            "configuration_data": {
+                "blocks": blocks,
+                "services": services
+            }
+        }
+        config_manager.update.return_value = {
+            "services": {
+                "started": [],
+                "stopped": [],
+                "added": [],
+                "modified": [],
+                "ignored": [],
+                "missing": []
+            },
+            "blocks": {
+                "added": [],
+                "modified": [],
+                "ignored": [],
+                "missing": []
+            }
+        }
+
+        handler.on_put(request, response)
+
+        # assert calls to update
+        self.assertEqual(config_manager.update.call_count, 1)
+        call_args = config_manager.update.call_args[0]
+        self.assertEqual(call_args[0], services)
+        self.assertEqual(call_args[1], blocks)
+        self.assertEqual(call_args[2], start_stop_services)
+        self.assertEqual(call_args[3], delete_missing)
+
+        # assert api response
+        response_body = json.loads(response.set_body.call_args[0][0])
+        self.assertDictEqual(response_body, config_manager.update.return_value)
 
     def test_hooks_called(self):
         # Verify hook is called when callback is executed
@@ -56,7 +114,8 @@ class TestConfigHandler(NIOWebTestCase):
         url_prefix = "url_prefix"
         instance_id = "instance_id"
         api_key = "api_key"
-        handler = ConfigHandler(url_prefix, instance_id, api_key)
+        handler = ConfigHandler(url_prefix, instance_id, api_key,
+                                None, None, None)
         handler._trigger_config_change_hook(CfgType.all)
 
         # handle async hook execution
